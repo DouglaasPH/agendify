5930483c9429d629a42886538b88c3f14144c437
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-08-14 23:28:46 -0300

    modifications.

--- frontend/src/pages/appointments/appointments.tsx
+++ frontend/src/pages/appointments/appointments.tsx
@@ -112,20 +112,20 @@ function AppointmentsPage() {
           </p>
         </section>
         <section className="w-full rounded-lg border border-[#E5E8EB] flex flex-col">
-          <div className="w-full flex flex-row items-center justify-between p-3 pl-5 pr-40">
-            <p className="font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
+          <div className="w-full flex flex-row items-center justify-between p-3 pl-5 gap-10">
+            <p className="w-1/5 font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
               Customer
             </p>
-            <p className="font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
+            <p className="w-1/5 font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
               Date
             </p>
-            <p className="font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
+            <p className="w-1/5 font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
               Time
             </p>
-            <p className="font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
+            <p className="w-1/5 font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
               Status
             </p>
-            <p className="font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#61738A]">
+            <p className="w-1/5 font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#61738A]">
               Actions
             </p>
           </div>
@@ -145,19 +145,19 @@ function AppointmentsPage() {
                   key={index}
                   className="mb-4 p-2 pl-5 pr-10 border-t border-[#E5E8EB] flex flex-row justify-between"
                 >
-                  <p className="font-inter font-regular text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
+                  <p className="w-1/5 font-inter font-regular text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417]">
                     {item.customer}
                   </p>
-                  <p className="font-inter font-regular text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#61738A]">
+                  <p className="w-1/5 font-inter font-regular text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#61738A]">
                     {item.date}
                   </p>
-                  <p className="font-inter font-regular text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#61738A]">
+                  <p className="w-1/5 font-inter font-regular text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#61738A]">
                     {item.start_time} - {item.end_time}
                   </p>
-                  <p className="capitalize text-center font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417] p-2 pl-1 xl:pl-2 pr-1 xl:pr-2 w-[7rem] sm:w-[8rem] xl:w-[9rem] bg-[#E8EDF5] rounded-lg">
+                  <p className="w-1/5 capitalize text-center font-inter font-medium text-[0.6rem] sm:text-[0.7rem] xl:text-[1rem] text-[#121417] p-2 pl-1 xl:pl-2 pr-1 xl:pr-2 bg-[#E8EDF5] rounded-lg">
                     {item.status}
                   </p>
-                  <div className="flex items-center gap-1">
+                  <div className="w-1/5 flex items-center gap-1">
                     <button className="font-inter font-bold text-[0.6rem] sm:text-[0.7rem] xl:text-[0.95rem] text-[#61738A] cursor-pointer hover:opacity-80">
                       Mark as completed
                     </button>
